c48154cf74fa285933538bd77aba6f9802d1fff1
Proglov <dev.mohammad.parvizi@gmail.com>
2024-12-14 19:00:56 +0330

    deleted the hello world!

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import appConfig from './config/app.config';
 import databaseConfig from './config/database.config';
 import environmentValidation from './config/environment.validation';
@@ -24,7 +26,9 @@ const ENV = process.env.NODE_ENV;
       useFactory: (configService: ConfigService) => ({
         uri: configService.get('database.URI')
       })
-    })
+    }),
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,17 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class AppService {
-
-    constructor(private readonly configService: ConfigService) { }
-
-    getHello(): string {
-
-        const env = this.configService.get('PORT')
-
-        console.log(process.env.PORT);
-        console.log(env);
-        return 'Hello World!';
-    }
-}
+export class AppService { }
