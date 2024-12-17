e542b87f5de2e626d2a789c051ee8577c30fa92a
Proglov <dev.mohammad.parvizi@gmail.com>
2024-12-17 20:11:23 +0330

    added JWT_SIGNATURE and JWT_ACCESS_TOKEN_TTL to the env validation

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -3,5 +3,7 @@ import * as Joi from "joi"
 export default Joi.object({
     NODE_ENV: Joi.string().valid('development', 'test', 'production').default('production'),
     PORT: Joi.number().port().default(4500),
-    MONGO_URI: Joi.string().required()
+    MONGO_URI: Joi.string().required(),
+    JWT_SIGNATURE: Joi.string().required(),
+    JWT_ACCESS_TOKEN_TTL: Joi.number().default(86400),
 })
