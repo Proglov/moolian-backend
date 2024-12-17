4c76879aa7e88b57eda0fb786538bd8afc04ac6b
Proglov <dev.mohammad.parvizi@gmail.com>
2024-12-17 23:54:49 +0330

    added access token guard

---
+++ src/auth/guards/access-token/access-token.guard.ts
@@ -0,0 +1,41 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Request } from 'express';
+import { JWTProvider } from 'src/auth/providers/jwt.provider';
+import { unauthorizedException } from 'src/common/errors';
+
+@Injectable()
+export class AccessTokenGuard implements CanActivate {
+
+  constructor(
+    private readonly jWTProvider: JWTProvider
+  ) { }
+
+  async canActivate(
+    context: ExecutionContext,
+  ): Promise<boolean> {
+
+    //extract request from the execution context
+    const request = context.switchToHttp().getRequest();
+
+    //extract the token from the request
+    const token = this.extractTokenFromTheRequest(request);
+
+    const unauthorizedMessage = 'شما احراز هویت نشده اید'
+
+    if (!token) throw unauthorizedException(unauthorizedMessage)
+
+    const jwtPayload = await this.jWTProvider.extractPayloadAndVerifyToken(token)
+
+    if (jwtPayload === null) throw unauthorizedException(unauthorizedMessage)
+
+    request['userInfo'] = jwtPayload
+
+    return true;
+  }
+
+
+  private extractTokenFromTheRequest(request: Request): string | undefined {
+    const [_, token] = request.headers.authorization?.split(' ') ?? []
+    return token
+  }
+}
