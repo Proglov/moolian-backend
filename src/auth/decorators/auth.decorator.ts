46f4e512e0619d93c8d01343dadbc352218053b1
Proglov <dev.mohammad.parvizi@gmail.com>
2024-12-18 17:11:31 +0330

    added ApiBearerAuth in the auth decorator

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,5 +1,18 @@
-import { SetMetadata } from '@nestjs/common';
+import { applyDecorators, SetMetadata } from '@nestjs/common';
 import { AuthType } from '../enums/auth-types';
 import { AUTH_TYPE_KEY } from 'src/common/constants';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
-export const Auth = (...authTypes: AuthType[]) => SetMetadata(AUTH_TYPE_KEY, authTypes);
+// Apply the ApiBearerAuth decorator if it includes bearer or admin
+const CustomApiBearerAuth = (...authTypes: AuthType[]) => {
+    return (target: any, key?: string, descriptor?: PropertyDescriptor) => {
+        if (authTypes.includes(AuthType.Bearer) || authTypes.includes(AuthType.Admin))
+            ApiBearerAuth()(target, key, descriptor);
+    };
+}
+
+export const Auth = (...authTypes: AuthType[]) =>
+    applyDecorators(
+        SetMetadata(AUTH_TYPE_KEY, authTypes),
+        CustomApiBearerAuth(...authTypes),
+    );
