0734ad2bfce704687578e6c5ca9a09d58bd38d8d
Proglov <dev.mohammad.parvizi@gmail.com>
2025-02-20 12:31:31 +0330

    updated the variables

--- src/config/environment.validation.ts
+++ src/config/environment.validation.ts
@@ -4,7 +4,8 @@ export default Joi.object({
     NODE_ENV: Joi.string().valid('development', 'test', 'production').default('production'),
     PORT: Joi.number().port().default(4500),
     MONGO_URI: Joi.string().required(),
-    JWT_SIGNATURE: Joi.string().required(),
+    JWT_ACCESS_SIGNATURE: Joi.string().required(),
     JWT_ACCESS_TOKEN_TTL: Joi.number().default(3600),
+    JWT_REFRESH_SIGNATURE: Joi.string().required(),
     JWT_REFRESH_TOKEN_TTL: Joi.number().default(86400),
 })
